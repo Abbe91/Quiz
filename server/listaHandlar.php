e0ade4fb25317045294b94baa1937bbcffcc45d6
Michelle Lanaro <michelle.lanaro@medieinstitutet.se>
2020-03-25 12:27:14 +0100

    Nu visas topplista i stigande ord.

--- server/listaHandlar.php
+++ server/listaHandlar.php
@@ -2,7 +2,7 @@
 function getlista() {
     include_once("./database.php");
     $database = new Database();
-    $query = $database->connection->prepare("SELECT username,points FROM users");
+    $query = $database->connection->prepare("SELECT username,points FROM users ORDER BY points ASC");
     $query->execute();
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
 
