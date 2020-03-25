0366133d3065535a8f89e564f17f993309fabe0e
Michelle Lanaro <michelle.lanaro@medieinstitutet.se>
2020-03-25 12:31:10 +0100

    bugs fixed

--- server/listaHandlar.php
+++ server/listaHandlar.php
@@ -2,7 +2,7 @@
 function getlista() {
     include_once("./database.php");
     $database = new Database();
-    $query = $database->connection->prepare("SELECT username,points FROM users ORDER BY points ASC");
+    $query = $database->connection->prepare("SELECT username,points FROM users WHERE points != 0 ORDER BY points DESC LIMIT 5");
     $query->execute();
     $result = $query->fetchAll(PDO::FETCH_ASSOC);
 
